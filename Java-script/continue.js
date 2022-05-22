00926a972ca99d5793f642d52517f685b7ba88cf
김유진 <w2117@e-mirim.hs.kr>
2022-05-22 15:24:12 +0900

    continue

---
+++ Java-script/continue.js
@@ -0,0 +1,15 @@
+let string = 'hello world';
+let seach = 'l';
+let count = 0;
+
+//문자열은 유사 배열이므로 for문으로 순회할 수 있다.
+for(let i = 0; i < string.length; i++){
+    //'l'이 아니면 현 지점에서 실행을 중단하고 반복문의 증감식으로 이동한다.
+    if(string[i] !== seach) continue;
+    count++; //continue문이 실행되면 이 문은 실행되지 않는다. 
+}
+console.log(count); //3
+
+//참고로 String.prototype.match 메서드를 사용해도 같은 동작을 한다.
+const regxp = new RegExp(seach, 'g');
+console.log(string.match(regxp).length); //3
---
+++ Java-script/continue2.js
@@ -0,0 +1,4 @@
+for(let i = 0; i < string.length; i++){
+    //'l'이면 카운트를 증가시킨다.
+    if(string[i]==seach) count++;
+}
