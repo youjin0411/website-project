3848537dd40e8b1fbf10fc3e845282bc4e6be62f
김유진 <w2117@e-mirim.hs.kr>
2022-05-22 15:06:19 +0900

    while + dowhile

---
+++ Java-script/WhileTest01.js
@@ -0,0 +1,17 @@
+let count = 0; 
+
+//count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+while(count < 3){
+    console.log(count); // 0,1,2
+    count++;
+}
+console.log();
+
+let count2 =0; 
+//무한루프
+while(true){
+    console.log(count);
+    count++;
+    //count가 3이면 코드 블록을 탈출한다.
+    if(count == 3 ) break;
+}
